ca9f17509646c327dc6ee2950883748cb88f3587
Ryan Berckmans <ryan.berckmans@gmail.com>
2011-06-17 03:03:07 -0400

    Seh: document and, or, not aliases

--- lib/seh.rb
+++ lib/seh.rb
@@ -5,14 +5,17 @@ require "seh/event"
 
 module Seh
   class << self
+    # alias for Seh::EventType::And
     def and( *types )
       EventType::And.new *types
     end
 
+    # alias for Seh::EventType::Or
     def or( *types )
       EventType::Or.new *types
     end
 
+    # alias for Seh::EventType::Not
     def not( type )
       EventType::Not.new type
     end
