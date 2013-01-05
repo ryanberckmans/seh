9bbc1e5a672aff88f5ab8a1dbe15b775c6c8686d
Ryan Berckmans <ryan.berckmans@gmail.com>
2013-01-05 13:31:26 -0500

    add EventTarget::Default, a default class mixing in EventTarget;  update event_target_spec

--- lib/seh/event_target.rb
+++ lib/seh/event_target.rb
@@ -6,14 +6,14 @@ module Seh
     def bind event_type=nil, &block
       raise "expected a block" unless block_given?
       @_binds ||= []
-      bind = EventBind.new( event_type, &block )
+      bind = EventBind.new event_type, &block
       @_binds << bind
       EventBindDisconnector.new ->{ @_binds.delete bind; nil }
     end
 
-    def bind_once( event_type=nil, &block )
+    def bind_once event_type=nil, &block
       return unless block_given?
-      bind = self.bind(event_type) { |event| bind.disconnect; block.call event }
+      bind_disconnector = self.bind(event_type) { |event| bind_disconnector.disconnect; block.call event }
     end
 
     # @private
@@ -27,12 +27,18 @@ module Seh
     # @private
     # Internal use only. Used in Seh::Event.
     # For each bind matching the passed event_types, yield the bind's callback
+    # @param event_types - Array of event types
     # @yield each bind callback matching the passed event types
     # @return nil
     def each_matching_callback event_types
       @_binds ||= []
       @_binds.each { |bind| yield bind.block if bind.event_type.match event_types }
       nil
+    end
+
+    # An empty class that includes EventTarget
+    class Default
+      include EventTarget
     end
   end
 end
--- spec/seh/event_target_spec.rb
+++ spec/seh/event_target_spec.rb
@@ -1,8 +1,88 @@
 require 'spec_helper'
 require 'seh/event_target'
+require 'seh/event_bind_disconnector'
 
 module Seh
-  describe EventTarget do
-    
+  module EventTarget
+    describe Default do
+      it { subject.should be_a(EventTarget) }
+
+      its(:observers) { should eq([]) }
+
+      it { subject.each_matching_callback([]).should be_nil }
+
+      it "adds a bind" do
+        event_type = :some_type
+        block = -> { callback! }
+        EventBind.should_receive(:new).with(event_type,&block).once.and_call_original
+        bind_disconnector = subject.bind event_type, &block
+        bind_disconnector.should be_a(EventBindDisconnector)
+      end
+
+      context "with added binds" do
+        before :each do
+          @event_type1 = :another_type
+          @block1 = -> { callback! }
+          @bind_disconnector1 = subject.bind @event_type1, &@block1
+
+          @event_type2 = :second_type
+          @block2 = -> { twocallback! }
+          @bind_disconnector2 = subject.bind @event_type2, &@block2
+
+          @event_type3 = :third_type
+          @block3 = ->event{ "do nothing" }
+          @bind_disconnector3 = subject.bind_once @event_type3, &@block3
+        end
+
+        it "yields the block for a matching bind in each_matching_callback" do
+          expected_blocks = []
+          subject.each_matching_callback([@event_type1]) { |block| expected_blocks << block }
+          expected_blocks.should include(@block1)
+          expected_blocks.size.should eq(1)
+        end
+
+        it "yields both blocks for the matching binds in each_matching_callback" do
+          expected_blocks = []
+          subject.each_matching_callback([@event_type1,@event_type2]) { |block| expected_blocks << block }
+          expected_blocks.should include(@block1)
+          expected_blocks.should include(@block2)
+          expected_blocks.size.should eq(2)
+        end
+
+        it "yields nothing when no binds match the event_types in each_matching_callback" do
+          expected_blocks = []
+          subject.each_matching_callback([:unknown_type]) { |block| expected_blocks << block }
+          expected_blocks.size.should eq(0)
+        end
+
+        it "disconnects bind3 after a single block invocation, since bind3 was connected using bind_once" do
+          expected_blocks = []
+          subject.each_matching_callback([@event_type3]) { |block| expected_blocks << block }
+          expected_blocks.should_not include(@block3) # @block3 would have been wrapped in another block during bind_once, so we don't expect to find it
+          expected_blocks.size.should eq(1) # the single block in the array is the new block wrapping @block3
+          puts expected_blocks[0]
+          expected_blocks[0].call
+
+          # After @block3 was called, we shouldn't expect to find it anymore
+          expected_blocks.clear
+          expected_blocks.size.should eq(0) # verify clear
+          subject.each_matching_callback([@event_type3]) { |block| expected_blocks << block }
+          expected_blocks.size.should eq(0)
+        end
+
+        context "with bind1 disconnected" do
+          before :each do
+            @bind_disconnector1.disconnect
+          end
+
+          it "yields only the second block in each_matching_callback, since the first was disconnected" do
+            expected_blocks = []
+            subject.each_matching_callback([@event_type1,@event_type2]) { |block| expected_blocks << block }
+            expected_blocks.should include(@block2)
+            expected_blocks.size.should eq(1)
+          end
+        end
+      end
+    end
   end
 end
